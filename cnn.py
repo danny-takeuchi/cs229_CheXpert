d7b0d38e6f9d51a57133fb55c3a731b8fac42bf5
Kevin Tran <30328540+kevtran23@users.noreply.github.com>
2019-06-07 09:04:52 -0700

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -124,7 +124,7 @@ net = torch.nn.DataParallel(DenseNet121(3)).cuda()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-for epoch in range(1):  # loop over the dataset multiple times
+for epoch in range(5):  # loop over the dataset multiple times
 
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
