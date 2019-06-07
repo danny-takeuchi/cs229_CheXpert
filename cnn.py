ab574b5c166457e559df4ec305fe7a0447933250
Kevin Tran <30328540+kevtran23@users.noreply.github.com>
2019-06-07 07:29:39 -0700

    Add files via upload

---
+++ cnn.py
@@ -0,0 +1,166 @@
+import numpy as np
+import sys
+import cv2
+import time
+import matplotlib.pyplot as plt
+import torch
+import torch.nn.functional as tfunc
+import torchvision.transforms as transforms
+from torch.utils.data.dataset import random_split
+from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+import torch.nn.functional as func
+import sklearn.metrics as metrics
+import random
+import csv
+from PIL import Image
+import torch
+from torch.utils.data import Dataset
+from train import CheXpertTrainer
+import models as mod
+from heatmap import HeatmapGenerator
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 3)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+
+class CheXpertViewDataSet(Dataset):
+    def __init__(self, image_list_file, transform=None):
+        """
+        image_list_file: path to the file containing images with corresponding labels.
+        transform: optional transform to be applied on a sample.
+        Upolicy: name the policy with regard to the uncertain labels
+        """
+        image_names = []
+        labels = []
+
+        with open(image_list_file, "r") as f:
+            csvReader = csv.reader(f)
+            next(csvReader, None)
+            k = 0
+            for line in csvReader:
+                k += 1
+                image_name = line[0]
+                if line[3] == "Lateral":
+                    label = 1
+                else:
+                    if line[4] == "AP":
+                        label = 2
+                    else:
+                        label = 0
+
+                image_names.append("../" + image_name)
+                labels.append(label)
+
+        self.image_names = image_names
+        self.labels = labels
+        self.transform = transform
+
+    def __getitem__(self, index):
+        """Take the index of item and returns the image and its labels"""
+
+        image_name = self.image_names[index]
+        image = Image.open(image_name).convert('RGB')
+        label = self.labels[index]
+        if self.transform is not None:
+            image = self.transform(image)
+        return image, label
+
+    def __len__(self):
+        return len(self.image_names)
+
+pathFileTrain = 'train-small.csv'
+# pathFileTrain = 'CheXpert-v1.0-small/train.csv'
+pathFileValid = '../CheXpert-v1.0-small/valid.csv'
+
+trBatchSize = 64
+
+# Parameters related to image transforms: size of the down-scaled image, cropped image
+imgtransResize = (320, 320)
+imgtransCrop = 224
+
+# Create DataLoaders
+normalize = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+transformList = []
+#transformList.append(transforms.Resize(imgtransCrop))
+transformList.append(transforms.RandomResizedCrop(imgtransCrop))
+transformList.append(transforms.RandomHorizontalFlip())
+transformList.append(transforms.ToTensor())
+transformList.append(normalize)
+transformSequence=transforms.Compose(transformList)
+
+#LOAD DATASET
+# dataset = CheXpertViewDataSet("CheXpert-v1.0-small/train.csv", transformSequence)
+dataset = CheXpertViewDataSet("train-small.csv", transformSequence)
+datasetTest, datasetTrain = random_split(dataset, [500, len(dataset) - 500])
+datasetValid = CheXpertViewDataSet(pathFileValid, transformSequence)
+
+trainloader = DataLoader(dataset=datasetTrain, batch_size=trBatchSize, shuffle=True,  num_workers=2, pin_memory=True)
+dataLoaderVal = DataLoader(dataset=datasetValid, batch_size=trBatchSize, shuffle=False, num_workers=2, pin_memory=True)
+testloader = DataLoader(dataset=datasetTest, num_workers=2, pin_memory=True)
+
+net = Net()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+        if inputs.shape[0] != trBatchSize:
+               continue
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
+
