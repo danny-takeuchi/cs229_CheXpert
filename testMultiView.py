18c5d61a860b9b4cb3a02d0baeb44936595bc29d
Ray Thai <raythai@DESKTOP-U5FSIFO.localdomain>
2019-06-10 14:43:54 -0700

    asdf

--- testMultiView.py
+++ testMultiView.py
@@ -26,7 +26,7 @@ class CheXpertTrainer():
             except ValueError:
                 pass
         return outAUROC
-    
+    """
     def testMulti(self, modelPA, modelAP, modelLat, dataLoaderTest, nnClassCount, paCheckpoint, apCheckpoint, latCheckpoint, class_names):   
         
         cudnn.benchmark = True
@@ -83,7 +83,8 @@ class CheXpertTrainer():
                         out = modelLat(varInput)
                     outPRED = torch.cat((outPRED, out), 0)
                     
-                    """"
+                    """
+                    """
                     predictionsForPatientStudy.append(out)
                 print(patientStudy)
                 for prediction in predictionsForPatientStudy:
@@ -92,8 +93,59 @@ class CheXpertTrainer():
                     outPRED = torch.cat((outPRED, newPrediction), 0) 
                 print(newPrediction)
                 """
+                """
                 if (patientCounter % 100) == 0:
                     print(patientCounter)
+        aurocIndividual = CheXpertTrainer.computeAUROC(self, outGT, outPRED, nnClassCount)
+        aurocMean = np.array(aurocIndividual).mean()
+        
+        print ('AUROC mean ', aurocMean)
+        
+        for i in range (0, len(aurocIndividual)):
+            print (class_names[i], ' ', aurocIndividual[i])
+        
+        return outGT, outPRED
+    """
+
+        def testMulti(self, modelPA, modelAP, modelLat, dataLoaderTest, nnClassCount, paCheckpoint, apCheckpoint, latCheckpoint, class_names):   
+        
+        cudnn.benchmark = True
+        print("In testMulti")
+        
+        if paCheckpoint != None and use_gpu:
+            modelPACheckpoint = torch.load(paCheckpoint)
+            modelAPCheckpoint = torch.load(apCheckpoint)
+            modelLatCheckpoint = torch.load(latCheckpoint)
+            # model.load_state_dict(modelCheckpoint)
+
+            modelPA.load_state_dict(modelPACheckpoint['state_dict'], strict=False)
+            modelAP.load_state_dict(modelAPCheckpoint['state_dict'], strict=False)
+            modelLat.load_state_dict(modelLatCheckpoint['state_dict'], strict=False)
+
+
+
+        if use_gpu:
+            outGT = torch.FloatTensor().cuda()
+            outPRED = torch.FloatTensor().cuda()
+        else:
+            outGT = torch.FloatTensor()
+            outPRED = torch.FloatTensor()
+       
+        modelPA.eval()
+        modelAP.eval()
+        modelLat.eval()
+        print("about to loop")
+        with torch.no_grad():
+            for i, (input, target) in enumerate(dataLoaderTest):
+
+                target = target.cuda()
+                outGT = torch.cat((outGT, target), 0).cuda()
+
+                bs, c, h, w = input.size()
+                varInput = input.view(-1, c, h, w)
+            
+                out = modelAP(varInput)
+                outPRED = torch.cat((outPRED, out), 0)
         aurocIndividual = CheXpertTrainer.computeAUROC(self, outGT, outPRED, nnClassCount)
         aurocMean = np.array(aurocIndividual).mean()
         
