64c762a9496480100393d4054f49de7aeca698a4
Ray Thai <raythai@DESKTOP-U5FSIFO.localdomain>
2019-06-10 01:27:39 -0700

    added multiview

---
+++ testMultiView.py
@@ -0,0 +1,53 @@
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import time
+import torch.backends.cudnn as cudnn
+import numpy as np
+from sklearn.metrics.ranking import roc_auc_score
+#from main import dataLoaderTest, dataLoaderTrain, dataLoaderVal, trMaxEpoch, nnClassCount
+
+use_gpu = torch.cuda.is_available()
+
+class CheXpertTrainer():
+    
+    def test(self, model1, model2, model3, dataLoaderTest, nnClassCount, checkpoint, class_names):   
+        
+        cudnn.benchmark = True
+        
+        if checkpoint != None and use_gpu:
+            modelCheckpoint = torch.load(checkpoint)
+            # model.load_state_dict(modelCheckpoint)
+
+            model.load_state_dict(modelCheckpoint['state_dict'], strict=False)
+
+
+        if use_gpu:
+            outGT = torch.FloatTensor().cuda()
+            outPRED = torch.FloatTensor().cuda()
+        else:
+            outGT = torch.FloatTensor()
+            outPRED = torch.FloatTensor()
+       
+        model.eval()
+        
+        with torch.no_grad():
+            for i, (input, target) in enumerate(dataLoaderTest):
+
+                target = target.cuda()
+                outGT = torch.cat((outGT, target), 0).cuda()
+
+                bs, c, h, w = input.size()
+                varInput = input.view(-1, c, h, w)
+            
+                out = model(varInput)
+                outPRED = torch.cat((outPRED, out), 0)
+        aurocIndividual = CheXpertTrainer.computeAUROC(self, outGT, outPRED, nnClassCount)
+        aurocMean = np.array(aurocIndividual).mean()
+        
+        print ('AUROC mean ', aurocMean)
+        
+        for i in range (0, len(aurocIndividual)):
+            print (class_names[i], ' ', aurocIndividual[i])
+        
+        return outGT, outPRED
