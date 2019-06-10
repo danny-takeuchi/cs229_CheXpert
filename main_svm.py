67c230c2db989e5465a1d453e9ae02ad0d97140d
Kevin Tran <30328540+kevtran23@users.noreply.github.com>
2019-06-10 05:40:58 -0700

    Update main_svm.py

--- main_svm.py
+++ main_svm.py
@@ -165,7 +165,7 @@ for i in range(nnClassCount):
         else:
             train_labels[randindex,i] = 1
     svclassifier.fit(train_features,train_labels[:,i])
-    test_pred = svclassifier.predict(test_features) 
+    test_pred = torch.from_numpy(svclassifier.predict(test_features))
     if(i == 0):
         test_pred_labels = test_pred
     else:
