c6fac03fe45f74f7b2b6353dff05c07afaaff23a
Kevin Tran <30328540+kevtran23@users.noreply.github.com>
2019-06-10 16:05:54 -0700

    Update main_svm.py

--- main_svm.py
+++ main_svm.py
@@ -84,7 +84,7 @@ dataset = CheXpertDataSet(pathFileTrain,transformSequence, policy=policy)
 
 
 #datasetTest, datasetTrain = random_split(dataset, [500, len(dataset) - 500])
-datasetTest, datasetTrain = random_split(dataset, [40, len(dataset) - 40])
+datasetTest, datasetTrain = random_split(dataset, [30, len(dataset) - 30])
 
 #datasetTest = torch.load("test.txt")
 
