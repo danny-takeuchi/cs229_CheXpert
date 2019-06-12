be1aad8f7b3d359478c65d7a91507a3ae01b6322
Kevin Tran <30328540+kevtran23@users.noreply.github.com>
2019-06-11 17:27:54 -0700

    Update main_tree.py

--- main_tree.py
+++ main_tree.py
@@ -197,48 +197,10 @@ for i in range(nnClassCount):
 
     except ValueError:
         pass
-aurocMean = np.array(outAUROC)
+aurocMean = np.array(outAUROC).mean()
 print(aurocMean, 'aurocMean')
-
-def make_meshgrid(x, y, h=.02):
-    x_min, x_max = x.min() - 1, x.max() + 1
-    y_min, y_max = y.min() - 1, y.max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-    return xx, yy
-
-def plot_contours(ax, clf, xx, yy, **params):
-    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-    Z = Z.reshape(xx.shape)
-    out = ax.contourf(xx, yy, Z, **params)
-    return out
-
-model = svm.SVC(kernel='linear')
-model.fit(train_features,train_labels[:,0])
-X = train_features
-y = train_labels[:,0]
-
-fig, ax = plt.subplots()
-# title for the plots
-title = ('Decision surface of linear SVC ')
-# Set-up grid for plotting.
-X0, X1 = X[:, 0], X[:, 1]
-xx, yy = make_meshgrid(X0, X1)
-
-plot_contours(ax, clf, xx, yy, cmap=plt.cm.coolwarm, alpha=0.8)
-ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
-ax.set_ylabel('y label here')
-ax.set_xlabel('x label here')
-ax.set_xticks(())
-ax.set_yticks(())
-ax.set_title(title)
-ax.legend()
-plt.show()
-
-
-#svclassifier.fit(train_features.detach().cpu().clone().numpy(),train_labels[:,i].detach().cpu().clone().numpy())
-#test_pred = svclassifier.predict(test_features.detach().cpu().clone().numpy())
-#print(confusion_matrix(y_test,y_pred))  
-#print(classification_report(y_test,y_pred)) 
+aurocMean1 = np.array(outAUROC)
+print(aurocMean1, 'aurocMean')
 
 
 
